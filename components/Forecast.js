6a862b8aa638c72fbb8959a0e740b65d1cef14cb
6010110185 <6010110185@psu.ac.th>
2021-07-22 21:49:36 +0700

    Connect

--- components/Forecast.js
+++ components/Forecast.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
 export default function Forecast(props) {
     return (
     <View >
-    <Text>{props.main}</Text>
-    <Text>{props.description}</Text>
+        <Text>{props.main}</Text>
+        <Text style={styles.BText}>{props.main}</Text>
+        <Text>{props.description}</Text>
     <View>
-    <Text>{props.temp}</Text>
-    <Text>°C</Text>
-    </View>
+            <Text>{props.temp}</Text>
+            <Text>°C</Text>
+            <Text style={styles.BText}>{props.temp}°C</Text>
+        </View>
     </View>
     );
    }
-   
+
+
+   const styles = StyleSheet.create({    //กำหนด backdrop style 
+    backdrop: {
+        flexDirection: 'column',
+        width: '100%',
+        height:'100%'
+    },
+    BText: {
+        fontSize: 20
+    },
+    BBlack:{
+        justifyContent: 'center', //center แนวแกนตั้ง
+        alignItems: 'center', //center แนวแกนนอน
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        height:'50%',
+        width:'100%'
+    }
+}) 
--- components/Weather.js
+++ components/Weather.js
@@ -1,26 +1,64 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text,ImageBackground,StyleSheet,View} from 'react-native'
 import Forecast from './Forecast';
+
 export default function Weather(props) {
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=e43eb205a5989742ed3dd683e08dd2f8`)
+            .then((response) => response.json())
+            .then((json) => {
+                setForecastInfo({
+                    main: json.weather[0].main,
+                    description: json.weather[0].description,
+                    temp: json.main.temp
+                    });
+                })
+            .catch((error) => {
+                console.warn(error);
+                });
+            }
+        }, [props.zipCode])
+
     const [forecastInfo, setForecastInfo] = useState({
-    main: '-',
-    description: '-',
-    temp: 0
-    })
+        main: '-',
+        description: '-',
+        main: 'main',
+        description: 'description',
+        temp: 0
+        })
     return (
         <View>
-        <ImageBackground source={require('../clear-blue-sky.jpeg')} style={styles.backdrop}>
-        <Text>Zip Code</Text>
-        <Text>{props.zipCode}</Text>
-        <Forecast {...forecastInfo} />
+            <ImageBackground source={require('../clear-blue-sky.jpeg')} style={styles.backdrop}>
+                <Text>Zip Code</Text>
+                <Text>{props.zipCode}</Text>
+            <View style={styles.BBlack}>
+                <Text style={styles.BText}>Zip Code is {props.zipCode}</Text>
+                <Forecast {...forecastInfo} />
+            </View>
         </ImageBackground>
         </View>
-        );
-       }
-       const styles = StyleSheet.create({
-        backdrop: {
+    );
+}
+
+const styles = StyleSheet.create({    
+    backdrop: {
         alignItems: 'center',
+        flexDirection: 'column',
         width: '100%',
         height: '100%'
-        },
-       });
+    },
+    BText: {
+        fontSize: 30
+    },
+
+    BBlack:{
+        justifyContent: 'center', //center แนวแกนตั้ง
+        alignItems: 'center', //center แนวแกนนอน
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        height:'50%',
+        width:'100%'
+    }
+
+}); 
